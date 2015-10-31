293c65a79323d1d8f681cf4f2d41919961cde55d
Jared Bennett <jarbenne@hwdsb.on.ca>
2015-10-31 13:54:13 -0400

    Create jp-publicize-slides.php

---
+++ jp-publicize-slides.php
@@ -0,0 +1,15 @@
+<?php
+/*
+ * Plugin Name: Publicize Presentations
+ * Plugin URI: https://github.com/hwdsbcommons/Publicize-Presentations/
+ * Description: Allow new Slides created with https://github.com/cgrymala/reveal-js-presentations/ to trigger a Publicize action using Jetpack Publicize
+ * Author: Jetpack Support 
+ * Author URI: https://jetpack.me/support/publicize/
+ * Version: 1.0
+ * License: GPL
+ */
+
+add_action('init', 'my_custom_init');
+function my_custom_init() {
+    add_post_type_support( 'slides', 'publicize' );
+}
