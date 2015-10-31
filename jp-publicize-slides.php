9a9ab111e2b3aac4b210f420518ae8b643261ed8
Jared Bennett <jarbenne@hwdsb.on.ca>
2015-10-31 16:07:25 -0400

    Update jp-publicize-slides.php
    Small license edit.

--- jp-publicize-slides.php
+++ jp-publicize-slides.php
@@ -6,7 +6,8 @@
  * Author: Jetpack Support 
  * Author URI: https://jetpack.me/support/publicize/
  * Version: 1.0
- * License: GPL
+ * License: GPLv2 or later
+ * License URI: http://www.gnu.org/licenses/gpl-2.0.html
  */
 
 add_action('init', 'my_custom_init');
